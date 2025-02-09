d2e71fe525fb91596873fe5cf8a52acc8aabaa2e
SarahMuAli <136401981+SarahMuAli@users.noreply.github.com>
2025-02-09 09:08:22 +0200

    .

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,45 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
+
+// GET all customers
+customersRouter.get(("/"), async (req, res) => {
+    const customers = await getCustomers();
+    res.json(customers);
+})
+
+// GET specific customer
+customersRouter.get("/:id", async (req, res) => {
+    const customer = await getCustomerDetail(req.params.id);
+    if (customer != null) {
+        res.json(customer);
+    } else {
+        res.status(404).json({ message: "customer not found" });
+    }
+})
+
+// Search for a specific customer
+customersRouter.get("/search/:query", async (req, res) => {
+    const customers = await searchCustomers(req.params.query);
+    if (customers != null) {
+        res.json(customers);
+    } else {
+        res.status(404).json({ message: "no customers found" });
+    }
+})
+
+// GET orders history //revisit to use then
+customersRouter.get("/:id/orders", async (req, res) => {
+    const customer = await getCustomerDetail(req.params.id);
+    if (customer != null) {
+        const orders = await getOrdersForCustomer(req.params.id);
+        if (orders.length > 0)
+            res.json(orders);
+        else
+            res.status(200).json("customer hasn't made orders yet")
+    } else {
+        res.status(404).json({ message: "no customer found" })
+    }
+})
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,13 @@
 import express from "express";
+import { getOrders } from "./orders.service";
+import { pagingRequestSchema } from "../types";
+import { validate } from "../../middleware/validation.middleware";
 
 export const ordersRouter = express.Router();
+
+// get orders - paging sets of 3
+ordersRouter.get("/", validate(pagingRequestSchema), async (req, res) => {
+    const data = pagingRequestSchema.parse(req);
+    const orders = getOrders(data.query.skip, data.query.take);
+    res.json(orders);
+})
